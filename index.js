67986cccd95c3c729769ee1cdc28acc7a757d19f
Natalie Montoya <nataliesbarron@gmail.com>
2017-10-22 18:26:27 -0600

    WIP started implementing JSON and localStorage but is not functioning

--- index.js
+++ index.js
@@ -12,15 +12,17 @@ function saveButtonEnabled() {
   }
 }
 
-
 function addIdea() {
   var id = $.now();
-  $(`<article id='${id}' class="idea-card">
+  var ideaToStore = $(`<article id='${id}' class="idea-card">
         <h2>${ideaName.value}</h2>
         <button id='${id}' class="delete-idea-button"><img src="images/delete.svg" alt="delete button" height='80'></button>
         <p class="idea-body">${ideaDetails.value}</p>
         <button id='${id}' class="up-vote-button"><img src="images/upvote.svg" alt="up vote button" height='80'></button>
         <button id='${id}' class="down-vote-button"><img src="images/downvote.svg" alt="down vote button" height="80"></button>
         <h3>quality: swill</h3>
-      </article>`).appendTo('.idea-section')
+      </article>`).appendTo('.idea-section');
+  var stringifiedIdea = JSON.stringify(ideaToStore);
+  localStorage.setItem('idea', stringifiedIdea);
+  console.log('it gets here');
 }
